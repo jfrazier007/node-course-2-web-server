9fec46b318b3c991cbeb21fc1674747c2c55611d
Jeff Frazier <jefffrazier@Supervisors-MBP.attlocal.net>
2018-01-15 19:46:00 -0500

    Added Projects page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) =>{
 	});
 });
 
+app.get('/projects', (req, res) =>{
+	res.render('projects.hbs', {
+		pageTitle: "Projects Page",	
+		projectMessage: "Welcome to the projects page!"
+	});
+});
+
 app.get('/bad', (req, res)=>{
 	res.send({
 		errorMessage: "Unable to handle request."
